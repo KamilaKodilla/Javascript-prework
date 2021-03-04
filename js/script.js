d5e623baef77d33dc802b9f4ce345070013b2f85
Kamila Kowalczyk-Radomska <kamila_kodilla@o2.pl>
2021-03-04 17:58:39 +0100

    Add result

--- js/script.js
+++ js/script.js
@@ -19,6 +19,8 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
+let playerMove = 'nieznany ruch';
+
 if(playerInput == '1'){ 
     playerMove = 'kamień'; 
 }
@@ -30,3 +32,34 @@ else if(playerInput == '3'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+
+ else if(computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  }
+  else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+      printMessage('Komputer wygrywa!');
+  }
+ 
+ if(computerMove == 'papier' && playerMove == 'kamień'){
+      printMessage('Komputer wygrywa!');
+  }
+  else if(computerMove == 'papier' && playerMove == 'papier'){
+      printMessage('Remis');
+  }
+  else if(computerMove == 'papier' && playerMove =='nożyce'){
+      printMessage('Ty wygrywasz!');
+  }
+ 
+ if(computerMove == 'nożyce' && playerMove == 'kamień'){
+      printMessage('Ty wygrywasz!');
+  }
+  else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+      printMessage('Remis!');
+  }
+  else if(computerMove == 'nożyce' && playerMove == 'papier'){
+      printMessage('Komputer wygrywa!');
+  }
